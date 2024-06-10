4937413f16dcb7a8b888d1a7f650a01ef7869dc5
LukeKellyGDEV <luke.kelly1993@hotmail.com>
2024-06-10 21:23:58 +0100

    Progress

--- Tetris/Tetris/Grid.cpp
+++ Tetris/Tetris/Grid.cpp
@@ -7,6 +7,7 @@ Grid::Grid()
 	NumCols = 10;
 	CellSize = 30;
 	Initialize();
+	Colours = GetCellColours();
 }
 
 void Grid::Initialize()
@@ -16,6 +17,18 @@ void Grid::Initialize()
 		for (int Column = 0; Column < NumCols; Column++)
 		{
 			GridArray[Row][Column] = 0;
+		}
+	}
+}
+
+void Grid::Draw()
+{
+	for (int Row = 0; Row < NumRows; Row++)
+	{
+		for (int Column = 0; Column < NumCols; Column++)
+		{
+			int CellValue = GridArray[Row][Column];
+			DrawRectangle(Column * CellSize, Row * CellSize, CellSize, CellSize, Colours[CellSize]);
 		}
 	}
 }
@@ -31,3 +44,17 @@ void Grid::Print()
 		std::cout << std::endl;
 	}
 }
+
+std::vector<Color> Grid::GetCellColours()
+{
+	Color DarkGrey = {26, 31, 40, 255};
+	Color Green = {47, 230, 23, 255};
+	Color Red = {232, 18, 18, 255};
+	Color Orange = {226, 116, 17, 255};
+	Color Yellow = {237, 234, 4, 255};
+	Color Purple = {166, 0, 247, 255};
+	Color Cyan = {21, 204, 209, 255};
+	Color Blue = {13, 64, 216, 255};
+
+	return {DarkGrey, Green, Red, Orange, Yellow, Purple, Cyan, Blue};
+}			
--- Tetris/Tetris/Grid.h
+++ Tetris/Tetris/Grid.h
@@ -1,15 +1,20 @@
 #pragma once
+#include <vector>
+#include <raylib.h>
 
 class Grid
 {
 public:
 	Grid();
 	void Initialize();
+	void Draw();
 	void Print();
 	int GridArray[20][10];
 private:
+	std::vector<Color> GetCellColours();
 	int NumRows;
 	int NumCols;
 	int CellSize;
+	std::vector<Color> Colours;
 };
 
